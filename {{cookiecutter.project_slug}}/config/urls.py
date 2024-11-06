2c2ec4993f59f89f51e17e9b021187ebe4bf0cd1
Dan Jacob <danjac2018@gmail.com>
2024-11-06 13:49:19 +0200

    fix: missing urls

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("account/", include("allauth.urls")),
     path("about/", views.about, name="about"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("accept-gdpr-cookies/", views.accept_gdpr_cookies, name="accept_gdpr_cookies"),
     path("robots.txt", views.robots, name="robots"),
     path("service-worker.js", views.service_worker, name="service_worker"),
     path("manifest.json", views.manifest, name="manifest"),
