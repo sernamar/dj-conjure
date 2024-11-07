61bab0dac1b47a507f91a90d58425d64801ad716
Dan Jacob <danjac2018@gmail.com>
2024-11-07 12:38:38 +0200

    fix: remove old url

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/views.py
@@ -8,8 +8,6 @@ from django.utils import timezone
 from django.views.decorators.cache import cache_control, cache_page
 from django.views.decorators.http import require_POST, require_safe
 
-from {{ cookiecutter.project_slug }}.manifest import get_assetlinks, get_manifest
-
 _CACHE_TIMEOUT: Final = 60 * 60 * 24 * 365
 
 
